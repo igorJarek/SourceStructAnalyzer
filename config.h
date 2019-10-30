5bfd5e256ef6646b3d645d3ba2ef5e2323d9f623
Igor <igij@o2.pl>
2019-10-30 12:07:46 +0100

    add View and view manipulation

--- config.h
+++ config.h
@@ -4,5 +4,6 @@
 #include <string>
 
 const std::string FONT_PATH = "courier-new.ttf";
+const float ZOOM_STEP {0.1f}; // 10 % zoom per "one click :)"
 
 #endif // CONFIG_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ int main()
     sf::RenderWindow window(sf::VideoMode(vMode.width, vMode.height), "Source Structure Analyzer " + VERSION_STRING);
     window.setFramerateLimit(60);
 
+    sf::View view(sf::FloatRect(0.f, 0.f, vMode.width, vMode.height));
+    window.setView(view);
+
+    sf::Vector2u resizeOldWindowSize {window.getSize()};
+    sf::Vector2f moveMousePressPoint {};
+    sf::Vector2f moveCenterViewPoint {};
+    bool leftMouseState = false;
+
     RowedFile rowedFile("main.tests");
     FunctionBlock functionalBlock(rowedFile);
     functionalBlock.setPosition(10, 10);
@@ -24,24 +32,80 @@ int main()
             if(event.type == sf::Event::Closed)
                 window.close();
 
+            if(event.type == sf::Event::Resized)
+            {
+                sf::View currentView{window.getView()};
+                currentView.setSize(event.size.width, event.size.height);
+                const sf::Vector2i currentWindowSize{resizeOldWindowSize.x - event.size.width, resizeOldWindowSize.y - event.size.height};
+                currentView.move(-currentWindowSize.x, -currentWindowSize.y);
+                resizeOldWindowSize.x = event.size.width;
+                resizeOldWindowSize.y = event.size.height;
+                window.setView(currentView);
+            }
+
+            if(event.type == sf::Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                {
+                    leftMouseState = true;
+                    moveMousePressPoint.x = event.mouseButton.x;
+                    moveMousePressPoint.y = event.mouseButton.y;
+                    moveCenterViewPoint = window.getView().getCenter();
+                }
+            }
+
+            if(event.type == sf::Event::MouseButtonReleased)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                    leftMouseState = false;
+            }
+
+            if(event.type == sf::Event::MouseMoved)
+            {
+                if(leftMouseState)
+                {
+                    sf::View currentView{window.getView()};
+                    currentView.setCenter(moveCenterViewPoint.x - (event.mouseMove.x - moveMousePressPoint.x), moveCenterViewPoint.y - (event.mouseMove.y - moveMousePressPoint.y));
+                    window.setView(currentView);
+                }
+            }
+
+            if(event.type == sf::Event::MouseWheelScrolled)
+            {
+                if(event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+                {
+                    float zoom {1.f + ZOOM_STEP};
+
+                    if (event.mouseWheelScroll.delta > 0)
+                        zoom = 1.f / zoom;
+
+                    sf::Vector2i pixel{ event.mouseWheelScroll.x, event.mouseWheelScroll.y };
+                    const sf::Vector2f beforeCoord{window.mapPixelToCoords(pixel)};
+                    sf::View view{window.getView()};
+                    view.zoom(zoom);
+                    window.setView(view);
+                    const sf::Vector2f afterCoord{ window.mapPixelToCoords(pixel)};
+                    const sf::Vector2f offsetCoords{beforeCoord - afterCoord};
+                    view.move(offsetCoords);
+                    window.setView(view);
+                }
+            }
+
             if(event.type == sf::Event::KeyPressed)
             {
+                sf::View currentView = window.getView();
+                sf::Vector2f centerPoint = currentView.getCenter();
+
                 if(event.key.code == sf::Keyboard::Up)
-                {
-
-                }
+                    currentView.setCenter(centerPoint.x, centerPoint.y - 10.f);
                 else if(event.key.code == sf::Keyboard::Right)
-                {
-
-                }
+                    currentView.setCenter(centerPoint.x - 10.f, centerPoint.y);
                 else if(event.key.code == sf::Keyboard::Down)
-                {
-
-                }
+                    currentView.setCenter(centerPoint.x, centerPoint.y+10.f);
                 else if(event.key.code == sf::Keyboard::Left)
-                {
+                    currentView.setCenter(centerPoint.x + 10.f, centerPoint.y);
 
-                }
+                window.setView(currentView);
             }
         }
 
